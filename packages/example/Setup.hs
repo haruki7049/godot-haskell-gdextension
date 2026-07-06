ba55731a2d199f6abf1d88668aeaa920511f8e84
haruki7049 <tontonkirikiri@gmail.com>
2026-07-07 06:10:31 +0900

    Update

--- packages/example/Setup.hs
+++ packages/example/Setup.hs
@@ -1,14 +1,23 @@
 module Main (main) where
 
 import Distribution.Simple
-import Distribution.Simple.Hpc (pathsToLibsArtifacts)
-import Distribution.Simple.LocalBuildInfo (allComponentsInBuildOrder)
+import Distribution.Simple.LocalBuildInfo (buildDir)
+import System.Directory (copyFile, createDirectoryIfMissing)
+import System.FilePath ((</>))
 
 main :: IO ()
 main =
   defaultMainWithHooks
     simpleUserHooks
-      { postBuild = \args buildFlags pkgDesc lbi -> do
-          let clbis = allComponentsInBuildOrder lbi
-          mapM_ (\clbi -> print clbi) clbis
+      { postBuild = \_ _ _ lbi -> do
+          -- Define the destination directory
+          let outDir = ".." </> ".." </> "example" </> "godot" </> "bin"
+          createDirectoryIfMissing True outDir
+
+          -- Define source and destination file paths
+          let srcLib = buildDir lbi </> "gdext-example" </> "libgdext-example.so"
+          let destLib = outDir </> "libgdext-example.so"
+
+          -- Copy the shared library
+          copyFile srcLib destLib
       }
